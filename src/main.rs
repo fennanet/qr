c18aef11b0dae1b0caf72ab8b5faa1745d43a10f
fabianovich <fabianvanacoley@gmail.com>
2026-08-18 13:32:34 +0200

    encode png

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::BufWriter;
+use png;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    encode_png();
+
+}
+
+
+fn encode_png() {
+    let path = Path::new(r"image.png");
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+    
+    let mut encoder = png::Encoder::new(w, 3, 1); // Width is 2 pixels and height is 1.
+    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_depth(png::BitDepth::Eight);
+    encoder.set_source_gamma(png::ScaledFloat::from_scaled(45455)); // 1.0 / 2.2, scaled by 100000
+    encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));     // 1.0 / 2.2, unscaled, but rounded
+    let source_chromaticities = png::SourceChromaticities::new(     // Using unscaled instantiation here
+        (0.31270, 0.32900),
+        (0.64000, 0.33000),
+        (0.30000, 0.60000),
+        (0.15000, 0.06000)
+    );
+    encoder.set_source_chromaticities(source_chromaticities);
+    let mut writer = encoder.write_header().unwrap();
+    
+    let data = [255, 0, 0, 255, 0, 0, 0, 255, 255, 0, 0, 255]; // An array containing a RGBA sequence. First pixel is red and second pixel is black.
+    writer.write_image_data(&data).unwrap(); // Save
 }
